e575893cea182a60ec79a4798508ec522a57fb96
Dwarika Kumar <dwarikabppimt202249@gmail.com>
2022-08-18 23:52:09 +0530

    first upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+    <div className="app">dfgg</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import './header.css'
+
+const Header = () => {
+  return (
+    <span className='header'>Filmystaan</span>
+  )
+}
+
+export default Header
